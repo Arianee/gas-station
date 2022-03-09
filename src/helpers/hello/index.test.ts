56baa38f760ef904b0103a80d33a82752e0556dd
stephane <s.delecroix@gmail.com>
2022-03-09 11:08:32 +0100

    feat(init): init fetch gas station

--- src/helpers/hello/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { hello } from './index';
-
-describe('first test', () => {
-  test('should return hello', () => {
-    expect(hello()).toBe('World');
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-var bodyParser = require('body-parser');
+import axios from 'axios';
 
-app.use(bodyParser.json());
+const CHAIN_GAS_STATION_V1 = {
+  1: 'https://api.anyblock.tools/ethereum/ethereum/mainnet/gasprice',
+  80001: 'https://gasstation-mainnet.matic.network',
+  137: 'https://gasstation-mainnet.matic.network/V1',
+  77: 'https://cert.arianee.org/gasStation/testnet.json',
+  99: 'https://cert.arianee.org/gasStation/mainnet.json'
+};
+const CHAIN_GAS_STATION_V2 = {
+  80001: 'https://gasstation-mainnet.matic.network/V2',
+  137: 'https://gasstation-mainnet.matic.network/v2',
+  77: 'https://cert.arianee.org/gasStation/V2/testnet.json',
+  99: 'https://cert.arianee.org/gasStation/V2/mainnet.json'
+};
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
+export interface GAS_STATION {
+  'safeLow': number,
+  'standard': number,
+  'fast': number,
+  'fastest': number,
+  'blockTime': number,
+  'blockNumber': number
+}
 
-app.listen(port, () => {
-  console.info(`Example app listening at http://localhost:${port}`);
-});
+export const fetchGasStation = async (chainId: string | number):Promise<GAS_STATION> => {
+  const url = CHAIN_GAS_STATION_V1[chainId.toString()];
+  if (!url) {
+    throw new Error(`this chain is not handle ${chainId}`);
+  }
+  const b = await axios.get(url);
+  return b.data;
+};
